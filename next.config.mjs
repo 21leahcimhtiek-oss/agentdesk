4ffb0299c891058de3b059f2a7a7b1b29b25e3aa
Raye Rayne <247046349+21leahcimhtiek-oss@users.noreply.github.com>
2026-04-04 17:17:07 -0700

    Add next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,23 +3,15 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["@node-rs/argon2"],
-  },
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "avatars.githubusercontent.com" },
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
-    ],
+    serverComponentsExternalPackages: ["@sentry/nextjs"],
   },
 };
 
 export default withSentryConfig(nextConfig, {
-  org: "aurora-rayes",
-  project: "agentdesk",
-  silent: !process.env.CI,
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: true,
   widenClientFileUpload: true,
-  tunnelRoute: "/monitoring",
   hideSourceMaps: true,
   disableLogger: true,
-  automaticVercelMonitors: true,
 });
