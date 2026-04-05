c7308a99ee8c3955c38bd8d805ebf20b7100aea6
Raye Rayne <247046349+21leahcimhtiek-oss@users.noreply.github.com>
2026-04-04 17:27:04 -0700

    Add Sentry client config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,11 +1,11 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN ?? process.env.SENTRY_DSN,
   tracesSampleRate: 0.1,
   debug: false,
   replaysOnErrorSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0.01,
   integrations: [
     Sentry.replayIntegration({
       maskAllText: true,
